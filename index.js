487fd1f93fb2e60e530be538fca0771fe6d2aa20
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-06-04 13:41:53 -0300

    Module 05 biggest city name and state

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ITEM01_PATH = './src/js/item01_createJsonForEachState';
 const ITEM02_PATH = './src/js/item02_totalCitiesByAcronym';
 const ITEM03_PATH = './src/js/item03_statesWithMoreCities';
 const ITEM04_PATH = './src/js/item04_statesWithLessCities';
+const ITEM05_PATH = './src/js/item05_biggestCityNameAndState.js';
 
 const fs = require('fs').promises;
 
@@ -9,23 +10,13 @@ const { createJsonStatesWithCities } = require(ITEM01_PATH);
 const totalcities = require(ITEM02_PATH);
 const { statesWithMoreCities, statesWithCities } = require(ITEM03_PATH);
 const statesWithLessCities = require(ITEM04_PATH);
+const { biggestCityNameAndState } = require(ITEM05_PATH);
 
 //createJsonStatesWithCities();
 //totalcities('PE').then((data) => console.log(data));
 //statesWithMoreCities(5);
 //statesWithLessCities(6);
-
-async function readCities(initials) {
-  let path = `src/json/estados/${initials}.json`;
-  let read = await fs.readFile(path);
-
-  let cities = JSON.parse(read);
-
-  //console.log(cities);
-  return cities; // {state: ['city', 'city', ...] }
-}
-
-//readCities('AC');
+//biggestCityNameAndState();
 
 async function citiesInAscendingOrderBySize(initials) {
   let cities = await readCities(initials); // {state: ['city', 'city', ...] }
@@ -64,39 +55,6 @@ async function smallestCityAndState(printOut = 'yes') {
 }
 
 //smallestCityAndState();
-
-async function citiesInDescendingOrderBySize(initials) {
-  let cities = await readCities(initials); // {state: ['city', 'city', ...] }
-  let state = Object.keys(cities)[0];
-  cities = cities[state]; // ['city', 'city', ...]
-
-  let citiesSorted = cities.sort((a, b) => {
-    return b.length - a.length;
-  });
-  citiesSortedWithState = {};
-  citiesSortedWithState[`${state}`] = citiesSorted;
-  //console.log(citiesSortedWithState);
-  return citiesSortedWithState; // { state: [ 'city', 'city', ... ] }
-}
-
-//citiesInDescendingOrderBySize('AC');
-
-async function biggestCityAndState() {
-  let dirContent = await fs.readdir('src/json/estados');
-  let biggestCityWithState = [];
-
-  for (const nameFileWithExtension of dirContent) {
-    let initials = nameFileWithExtension.substr(0, 2);
-    let citiesSorted = await citiesInDescendingOrderBySize(initials);
-    let state = Object.keys(citiesSorted)[0]; // state
-    let biggestCity = citiesSorted[`${state}`][0]; // city
-    // prettier-ignore
-    biggestCityWithState = [...biggestCityWithState, `${biggestCity}-${initials}`];
-  }
-  console.log(biggestCityWithState);
-}
-
-//biggestCityAndState();
 
 async function smallestCityOfAll() {
   let smallestCities = await smallestCityAndState('no');
---
+++ src/js/item05_biggestCityNameAndState.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+
+async function readCities(initials) {
+  let path = `src/json/estados/${initials}.json`;
+  let read = await fs.readFile(path);
+
+  let cities = JSON.parse(read);
+
+  //console.log(cities);
+  return cities; // {state: ['city', 'city', ...] }
+}
+
+////readCities('AC');
+
+async function citiesInDescendingOrderBySize(initials) {
+  let cities = await readCities(initials); // {state: ['city', 'city', ...] }
+  let state = Object.keys(cities)[0];
+  cities = cities[state]; // ['city', 'city', ...]
+
+  let citiesSorted = cities.sort((a, b) => {
+    return b.length - a.length;
+  });
+  citiesSortedWithState = {};
+  citiesSortedWithState[`${state}`] = citiesSorted;
+  //console.log(citiesSortedWithState);
+  return citiesSortedWithState; // { state: [ 'city', 'city', ... ] }
+}
+
+////citiesInDescendingOrderBySize('AC');
+
+async function biggestCityNameAndState() {
+  let dirContent = await fs.readdir('src/json/estados');
+  let biggestCityWithState = [];
+
+  for (const nameFileWithExtension of dirContent) {
+    let initials = nameFileWithExtension.substr(0, 2);
+    let citiesSorted = await citiesInDescendingOrderBySize(initials);
+    let state = Object.keys(citiesSorted)[0]; // state
+    let biggestCity = citiesSorted[`${state}`][0]; // city
+    // prettier-ignore
+    biggestCityWithState = [...biggestCityWithState, `${biggestCity}-${initials}`];
+  }
+  biggestCityWithState.sort((a, b) => {
+    return b.length - a.length;
+  });
+  console.log(biggestCityWithState);
+}
+
+module.exports.biggestCityNameAndState = biggestCityNameAndState;
+module.exports.readCities = readCities;

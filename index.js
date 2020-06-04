d058f9ef1bda5d2bc054a3f942d2af0fdabc237c
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-06-04 13:41:47 -0300

    Create basic logic to trasnform json states

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-fs.readFile('./src/json/Estados.json', (err, data) => {
-  console.log(JSON.parse(data));
+// Return a state bject
+function states() {
+  return fs.readFile('./src/json/Estados.json', 'utf-8').then((data) => {
+    return Array.from(JSON.parse(data));
+  });
+}
+
+// Return a cities object
+function cities() {
+  return fs.readFile('./src/json/Cidades.json', 'utf-8').then((data) => {
+    return Array.from(JSON.parse(data));
+  });
+}
+
+states().then((states) => {
+  let { ID: idState, Sigla: initialsState, Nome: stateName } = states[0];
+
+  let nameFile = `${initialsState}.json`;
+  let pathWrite = `./src/json/${nameFile}`;
+
+  cities().then((cities) => {
+    let data = { [`${stateName}`]: [] };
+
+    cities.forEach((city) => {
+      let { ID: idCity, Nome: cityName, Estado: idCityState } = city;
+      if (idCityState === idState) {
+        data[`${stateName}`].push(cityName);
+      }
+    });
+    fs.writeFile(pathWrite, JSON.stringify(data));
+    //console.log(cities);
+  });
 });
-
-console.log('Funcionou!');

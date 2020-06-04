fd1eaa80bd3042e3178005a17ce7db0f885ce017
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-06-04 13:41:53 -0300

    Module 06 smallest city name and state

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ITEM02_PATH = './src/js/item02_totalCitiesByAcronym';
 const ITEM03_PATH = './src/js/item03_statesWithMoreCities';
 const ITEM04_PATH = './src/js/item04_statesWithLessCities';
 const ITEM05_PATH = './src/js/item05_biggestCityNameAndState.js';
+const ITEM06_PATH = './src/js/item06_smallestCityNameAndState.js';
 
 const fs = require('fs').promises;
 
@@ -11,6 +12,7 @@ const totalcities = require(ITEM02_PATH);
 const { statesWithMoreCities, statesWithCities } = require(ITEM03_PATH);
 const statesWithLessCities = require(ITEM04_PATH);
 const { biggestCityNameAndState } = require(ITEM05_PATH);
+const { smallestCityNameAndState } = require(ITEM06_PATH);
 
 //createJsonStatesWithCities();
 //totalcities('PE').then((data) => console.log(data));
@@ -18,43 +20,7 @@ const { biggestCityNameAndState } = require(ITEM05_PATH);
 //statesWithLessCities(6);
 //biggestCityNameAndState();
 
-async function citiesInAscendingOrderBySize(initials) {
-  let cities = await readCities(initials); // {state: ['city', 'city', ...] }
-  let state = Object.keys(cities)[0];
-  cities = cities[state]; // ['city', 'city', ...]
-
-  let citiesSorted = cities.sort((a, b) => {
-    return a.length - b.length;
-  });
-  citiesSortedWithState = {};
-  citiesSortedWithState[`${state}`] = citiesSorted;
-  //console.log(citiesSortedWithState);
-  return citiesSortedWithState; // { state: [ 'city', 'city', ... ] }
-}
-
-//citiesInAscendingOrderBySize('AC');
-
-async function smallestCityAndState(printOut = 'yes') {
-  let dirContent = await fs.readdir('src/json/estados');
-  let smallestCityWithState = [];
-
-  for (const nameFileWithExtension of dirContent) {
-    let initials = nameFileWithExtension.substr(0, 2);
-    let citiesSorted = await citiesInAscendingOrderBySize(initials);
-    let state = Object.keys(citiesSorted)[0]; // state
-    let smallestCity = citiesSorted[`${state}`][0]; // city
-    // prettier-ignore
-    smallestCityWithState = [...smallestCityWithState, `${smallestCity}-${initials}`];
-  }
-  if (printOut === 'no') {
-    return smallestCityWithState;
-  } else if (printOut === 'yes' || '') {
-    console.log(smallestCityWithState);
-    return smallestCityWithState;
-  }
-}
-
-//smallestCityAndState();
+smallestCityNameAndState();
 
 async function smallestCityOfAll() {
   let smallestCities = await smallestCityAndState('no');
---
+++ src/js/item06_smallestCityNameAndState.js
@@ -0,0 +1,46 @@
+const fs = require('fs').promises;
+
+const { readCities } = require('./item05_biggestCityNameAndState');
+
+async function citiesInAscendingOrderBySize(initials) {
+  let cities = await readCities(initials); // {state: ['city', 'city', ...] }
+  let state = Object.keys(cities)[0];
+  cities = cities[state]; // ['city', 'city', ...]
+
+  let citiesSorted = cities.sort((a, b) => {
+    return a.length - b.length;
+  });
+  citiesSortedWithState = {};
+  citiesSortedWithState[`${state}`] = citiesSorted;
+  //console.log(citiesSortedWithState);
+  return citiesSortedWithState; // { state: [ 'city', 'city', ... ] }
+}
+
+////citiesInAscendingOrderBySize('AC');
+
+async function smallestCityNameAndState(printOut = 'yes') {
+  let dirContent = await fs.readdir('src/json/estados');
+  let smallestCityWithState = [];
+
+  for (const nameFileWithExtension of dirContent) {
+    let initials = nameFileWithExtension.substr(0, 2);
+    let citiesSorted = await citiesInAscendingOrderBySize(initials);
+    let state = Object.keys(citiesSorted)[0]; // state
+    let smallestCity = citiesSorted[`${state}`][0]; // city
+    // prettier-ignore
+    smallestCityWithState = [...smallestCityWithState, `${smallestCity}-${initials}`];
+  }
+
+  smallestCityWithState.sort((a, b) => {
+    return b.length - a.length;
+  });
+
+  if (printOut === 'no') {
+    return smallestCityWithState;
+  } else if (printOut === 'yes' || '') {
+    console.log(smallestCityWithState);
+    return smallestCityWithState;
+  }
+}
+
+module.exports.smallestCityNameAndState = smallestCityNameAndState;
